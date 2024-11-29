7432911c4b4743bb0bfe5149ff4702365926d848
AzathREJG13 <rafaeleduardo130201@gmail.com>
2024-11-28 21:13:11 -0600

    Template Home

--- apps/App1/admin.py
+++ apps/App1/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from apps.App1 import models
 # Register your models here.
+
+
--- apps/App1/models.py
+++ apps/App1/models.py
@@ -1,3 +1,4 @@
 from django.db import models
-
+from django.contrib.auth.hashers import make_password, check_password
 # Create your models here.
+
